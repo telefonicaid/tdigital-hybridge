e32e0ac6e5e80915d713e0f384b39db987293908
David García Sánchez <dgarcia@aurigae.com>
2013-10-29 10:55:50 +0100

    EDIT coding guidelines

--- android/Hybridge/src/com/pdi/hybridge/HybridgeBroadcaster.java
+++ android/Hybridge/src/com/pdi/hybridge/HybridgeBroadcaster.java
@@ -16,18 +16,18 @@ import android.webkit.WebView;
 import com.pdi.hybridge.HybridgeConst.Event;
 
 public class HybridgeBroadcaster extends Observable {
-	
-	private static HybridgeBroadcaster instance;
-	
-	private boolean isInitialized = false;
-	
-	private final String TAG = "HybridgeBroadcaster";
-	
-	private HashMap<Integer, AsyncTask<JSONObject, Void, JSONObject>> currents;
-	
-	public HybridgeBroadcaster () {
-	    currents =  new HashMap<Integer, AsyncTask<JSONObject,Void,JSONObject>>();
-	}
+
+    private static HybridgeBroadcaster instance;
+
+    private boolean isInitialized = false;
+
+    private final String TAG = "HybridgeBroadcaster";
+
+    private HashMap<Integer, AsyncTask<JSONObject, Void, JSONObject>> currents;
+
+    public HybridgeBroadcaster () {
+        currents =  new HashMap<Integer, AsyncTask<JSONObject,Void,JSONObject>>();
+    }
 
     public static HybridgeBroadcaster getInstance () {
         if (instance == null) {
@@ -50,79 +50,79 @@ public class HybridgeBroadcaster extends Observable {
         isInitialized = true;
     }
 
-	public void firePause (WebView view) {
-		HybridgeConst.Event event = HybridgeConst.Event.PAUSE;
-		notifyObservers(event);
-		fireJavascriptEvent(view, event, null);
-	}
-	
-	public void fireResume (WebView view) {
-		HybridgeConst.Event event = HybridgeConst.Event.RESUME;
-		notifyObservers(event);
-		fireJavascriptEvent(view, event, null);
-	}
-	
-	public void fireMessage (WebView view, JSONObject data) {
-		HybridgeConst.Event event = HybridgeConst.Event.MESSAGE;
-		notifyObservers(event);
-		fireJavascriptEvent(view, event, data);
-	}
-	
+    public void firePause (WebView view) {
+        HybridgeConst.Event event = HybridgeConst.Event.PAUSE;
+        notifyObservers(event);
+        fireJavascriptEvent(view, event, null);
+    }
+
+    public void fireResume (WebView view) {
+        HybridgeConst.Event event = HybridgeConst.Event.RESUME;
+        notifyObservers(event);
+        fireJavascriptEvent(view, event, null);
+    }
+
+    public void fireMessage (WebView view, JSONObject data) {
+        HybridgeConst.Event event = HybridgeConst.Event.MESSAGE;
+        notifyObservers(event);
+        fireJavascriptEvent(view, event, data);
+    }
+
     public void fireReady (WebView view, JSONObject data) {
         HybridgeConst.Event event = HybridgeConst.Event.READY;
         notifyObservers(event);
         fireJavascriptEvent(view, event, data);
     }
-	
-	public void fireJavascriptEvent (WebView view, Event event, JSONObject data) {
-		if (isInitialized) {
-			String json = data != null ? data.toString() : "{}";
-			StringBuffer js = new StringBuffer("HybridgeGlobal.fireEvent(\"");
-			js.append(event.getJsName()).append("\",").append(json).append(");");
-			runJsInWebView(view, js.toString());
-		}
-	}
 
-	public void runJsInWebView (final WebView view, final String js) {
-	    new Handler(Looper.getMainLooper()).post(
-	            new Runnable() {
-	                @Override
-	                public void run() {
-	                    view.loadUrl("javascript:(function(){" + js + "})()");
-	                }
-	            });
-	}
-	
-	public void updateState (JSONObject data) {
-		this.setChanged();
-		this.notifyObservers(data);
-		Log.d(TAG, data.toString());
-	}
-	
-	@SuppressWarnings("unchecked")
-	@Override
+    public void fireJavascriptEvent (WebView view, Event event, JSONObject data) {
+        if (isInitialized) {
+            String json = data != null ? data.toString() : "{}";
+            StringBuffer js = new StringBuffer("HybridgeGlobal.fireEvent(\"");
+            js.append(event.getJsName()).append("\",").append(json).append(");");
+            runJsInWebView(view, js.toString());
+        }
+    }
+
+    public void runJsInWebView (final WebView view, final String js) {
+        new Handler(Looper.getMainLooper()).post(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        view.loadUrl("javascript:(function(){" + js + "})()");
+                    }
+                });
+    }
+
+    public void updateState (JSONObject data) {
+        this.setChanged();
+        this.notifyObservers(data);
+        Log.d(TAG, data.toString());
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
     public void addObserver(Observer observer) {
-	    super.addObserver(observer);
-	    Class<?> clazz = observer.getClass().getSuperclass();
-	    if (clazz != null && clazz == android.os.AsyncTask.class) {
-	        int hashCode = observer.hashCode();
-	        AsyncTask<JSONObject, Void, JSONObject> current = currents.get(hashCode); 
-	        if (current != null) {
-	            if (current.cancel(true)) {
-	                currents.remove(hashCode);
-	            }
-	        }
-	        currents.put(hashCode, (AsyncTask<JSONObject, Void, JSONObject>) observer);
-	    }
-	}
-	
-	@Override
+        super.addObserver(observer);
+        Class<?> clazz = observer.getClass().getSuperclass();
+        if (clazz != null && clazz == android.os.AsyncTask.class) {
+            int hashCode = observer.hashCode();
+            AsyncTask<JSONObject, Void, JSONObject> current = currents.get(hashCode); 
+            if (current != null) {
+                if (current.cancel(true)) {
+                    currents.remove(hashCode);
+                }
+            }
+            currents.put(hashCode, (AsyncTask<JSONObject, Void, JSONObject>) observer);
+        }
+    }
+
+    @Override
     public void deleteObserver(Observer observer) {
-	    super.deleteObserver(observer);
+        super.deleteObserver(observer);
         Class<?> clazz = observer.getClass().getSuperclass();
         if (clazz != null && clazz == android.os.AsyncTask.class) {
             int hashCode = observer.hashCode();
             currents.remove(hashCode);
         }	    
-	}
+    }
 }
