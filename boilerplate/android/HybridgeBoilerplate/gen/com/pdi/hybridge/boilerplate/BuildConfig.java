5904660b506d336904a3e08bca368aada4e80ea9
David García Sánchez <dgarcia@aurigae.com>
2013-11-11 13:18:39 +0100

    EDIT add Android boilerplate

---
+++ boilerplate/android/HybridgeBoilerplate/gen/com/pdi/hybridge/boilerplate/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.pdi.hybridge.boilerplate;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ boilerplate/android/HybridgeBoilerplate/src/com/pdi/hybridge/boilerplate/JsActionImpl.java
@@ -0,0 +1,28 @@
+
+package com.pdi.hybridge.boilerplate;
+
+import com.pdi.hybridge.JsAction;
+import com.pdi.hybridge.boilerplate.action.InitTask;
+
+@SuppressWarnings("rawtypes")
+public enum JsActionImpl implements JsAction {
+
+    INIT(InitTask.class);
+
+    private Class task;
+
+    private JsActionImpl(Class task) {
+        this.setTask(task);
+    }
+
+    @Override
+    public Class getTask() {
+        return task;
+    }
+
+    @Override
+    public void setTask(Class task) {
+        this.task = task;
+    }
+
+}
