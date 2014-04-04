75d30d998f61848595514b017d1f934002f85792
Guillermo Gonzalez <guillermo.gonzalez@me.com>
2014-04-04 12:42:37 +0200

    Implement HYBURLProtocol

--- ios/Hybridge/Hybridge/HYBURLProtocol.m
+++ ios/Hybridge/Hybridge/HYBURLProtocol.m
@@ -17,7 +17,7 @@ static NSString * const kHTTPOptionsMethod = @"OPTIONS";
 
 + (BOOL)canInitWithRequest:(NSURLRequest *)request {
     NSString *host = [[[request URL] host] lowercaseString];
-    return [host isEqualToString:HYBHostName];
+    return [host isEqualToString:HYBHostName] && ([HYBBridge activeBridge] != nil);
 }
 
 + (NSURLRequest *)canonicalRequestForRequest:(NSURLRequest *)request {
@@ -25,7 +25,27 @@ static NSString * const kHTTPOptionsMethod = @"OPTIONS";
 }
 
 - (void)startLoading {
-    // TODO: implement
+    NSHTTPURLResponse *response = nil;
+    
+    if (![[self.request HTTPMethod] isEqualToString:kHTTPOptionsMethod]) {
+        NSString *action = [[[self.request URL] pathComponents] firstObject];
+        
+        NSDictionary *headers = [self.request allHTTPHeaderFields];
+        NSData *data = [headers[@"data"] dataUsingEncoding:NSUTF8StringEncoding];
+        id JSONObject = [NSJSONSerialization JSONObjectWithData:data options:0 error:NULL];
+        
+        response = [[HYBBridge activeBridge] sendAction:action data:JSONObject];
+    }
+    
+    if (!response) {
+        response = [NSHTTPURLResponse hyb_responseWithURL:[self.request URL] statusCode:200];
+    }
+    
+    [self.client URLProtocol:self didReceiveResponse:response cacheStoragePolicy:NSURLCacheStorageNotAllowed];
+    [self.client URLProtocolDidFinishLoading:self];
+}
+
+- (void)stopLoading {
 }
 
 @end
