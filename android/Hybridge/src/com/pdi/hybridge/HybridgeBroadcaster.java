ca09d1bb4e0d6b2387f8d1f1034db525c3cc8ace
davidgarsan <davidgarsan@gmail.com>
2014-06-03 11:22:49 +0200

    EDIT publish flag getter method and format code

--- android/Hybridge/src/com/pdi/hybridge/HybridgeBroadcaster.java
+++ android/Hybridge/src/com/pdi/hybridge/HybridgeBroadcaster.java
@@ -6,13 +6,6 @@
 
 package com.pdi.hybridge;
 
-import java.util.HashMap;
-import java.util.Observable;
-import java.util.Observer;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-
 import android.annotation.SuppressLint;
 import android.os.AsyncTask;
 import android.util.Log;
@@ -20,6 +13,13 @@ import android.webkit.WebView;
 import android.webkit.WebView.HitTestResult;
 
 import com.pdi.hybridge.HybridgeConst.Event;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Observable;
+import java.util.Observer;
 
 public class HybridgeBroadcaster extends Observable {
 
@@ -30,12 +30,12 @@ public class HybridgeBroadcaster extends Observable {
     private final String TAG = "HybridgeBroadcaster";
 
     private StringBuffer jsBuffer;
-    
+
     private HashMap<Integer, AsyncTask<JSONObject, Void, JSONObject>> currents;
 
     @SuppressLint("UseSparseArrays")
     public HybridgeBroadcaster() {
-        currents = new HashMap<Integer, AsyncTask<JSONObject,Void,JSONObject>>();
+        currents = new HashMap<Integer, AsyncTask<JSONObject, Void, JSONObject>>();
         jsBuffer = new StringBuffer("");
     }
 
@@ -47,69 +47,69 @@ public class HybridgeBroadcaster extends Observable {
     }
 
     public void initJs(WebView view, JSONArray actions, JSONArray events) {
-        runJsInWebView(view, "window.HybridgeGlobal || setTimeout(function () {" +
-                "window.HybridgeGlobal = {" +
-                "  isReady : true" +
-                ", version : " + HybridgeConst.VERSION +
-                ", actions : " + actions.toString() +
-                ", events : " + events.toString() +
-                "};" +
-                "window.$ && $('#hybridgeTrigger').toggleClass('switch');" +
-                "},0)"
-                );
+        runJsInWebView(view, "window.HybridgeGlobal || setTimeout(function () {"
+                + "window.HybridgeGlobal = {" + //
+                "  isReady : true" + //
+                ", version : " + HybridgeConst.VERSION + //
+                ", actions : " + actions.toString() + //
+                ", events : " + events.toString() + //
+                "};" //
+                + "window.$ && $('#hybridgeTrigger').toggleClass('switch');" + //
+                "},0)" //
+        );
         isInitialized = true;
     }
 
     public void firePause(WebView view) {
-        HybridgeConst.Event event = HybridgeConst.Event.PAUSE;
+        final HybridgeConst.Event event = HybridgeConst.Event.PAUSE;
         notifyObservers(event);
         fireJavascriptEvent(view, event, null);
     }
 
     public void fireResume(WebView view) {
-        HybridgeConst.Event event = HybridgeConst.Event.RESUME;
+        final HybridgeConst.Event event = HybridgeConst.Event.RESUME;
         notifyObservers(event);
         fireJavascriptEvent(view, event, null);
     }
 
-    public void fireMessage (WebView view, JSONObject data) {
-        HybridgeConst.Event event = HybridgeConst.Event.MESSAGE;
+    public void fireMessage(WebView view, JSONObject data) {
+        final HybridgeConst.Event event = HybridgeConst.Event.MESSAGE;
         notifyObservers(event);
         fireJavascriptEvent(view, event, data);
     }
 
     public void fireReady(WebView view, JSONObject data) {
-        HybridgeConst.Event event = HybridgeConst.Event.READY;
+        final HybridgeConst.Event event = HybridgeConst.Event.READY;
         notifyObservers(event);
         fireJavascriptEvent(view, event, data);
     }
 
     public void fireJavascriptEvent(final WebView view, final Event event, final JSONObject data) {
         if (isInitialized) {
-            view.post(
-                new Runnable() {
-                    @Override
-                    public void run() {
-                        WebView.HitTestResult hitTestResult = ((WebView)view).getHitTestResult();
-                        String prejs = "";
-                        String json = data != null ? data.toString() : "{}";
-                        StringBuffer js = new StringBuffer("HybridgeGlobal.fireEvent(\"");
-                        js.append(event.getJsName()).append("\",").append(json).append(");");
+            view.post(new Runnable() {
+                @Override
+                public void run() {
+                    final WebView.HitTestResult hitTestResult = view.getHitTestResult();
+                    String prejs = "";
+                    final String json = data != null ? data.toString() : "{}";
+                    final StringBuffer js = new StringBuffer("HybridgeGlobal.fireEvent(\"");
+                    js.append(event.getJsName()).append("\",").append(json).append(");");
 
-                        if (hitTestResult == null || hitTestResult.getType() != HitTestResult.EDIT_TEXT_TYPE) {
-                            if(jsBuffer.length() != 0) {
-                                prejs = jsBuffer.append(js.toString()).toString();
-                                runJsInWebView(view, prejs);
-                                jsBuffer = new StringBuffer("");
-                            } else {
-                                runJsInWebView(view, js.toString());
-                            }
+                    if (hitTestResult == null
+                            || hitTestResult.getType() != HitTestResult.EDIT_TEXT_TYPE) {
+                        if (jsBuffer.length() != 0) {
+                            prejs = jsBuffer.append(js.toString()).toString();
+                            runJsInWebView(view, prejs);
+                            jsBuffer = new StringBuffer("");
                         } else {
-                            Log.d(TAG, "Defer javascript message, user is entering text");
-                            jsBuffer.append(js.toString());
+                            runJsInWebView(view, js.toString());
                         }
+                    } else {
+                        Log.d(TAG, "Defer javascript message, user is entering text");
+                        jsBuffer.append(js.toString());
                     }
-                });
+                }
+            });
         }
     }
 
@@ -127,10 +127,10 @@ public class HybridgeBroadcaster extends Observable {
     @Override
     public void addObserver(Observer observer) {
         super.addObserver(observer);
-        Class<?> clazz = observer.getClass().getSuperclass();
+        final Class<?> clazz = observer.getClass().getSuperclass();
         if (clazz != null && clazz == android.os.AsyncTask.class) {
-            int hashCode = observer.hashCode();
-            AsyncTask<JSONObject, Void, JSONObject> current = currents.get(hashCode); 
+            final int hashCode = observer.hashCode();
+            final AsyncTask<JSONObject, Void, JSONObject> current = currents.get(hashCode);
             if (current != null) {
                 if (current.cancel(true)) {
                     currents.remove(hashCode);
@@ -143,10 +143,20 @@ public class HybridgeBroadcaster extends Observable {
     @Override
     public void deleteObserver(Observer observer) {
         super.deleteObserver(observer);
-        Class<?> clazz = observer.getClass().getSuperclass();
+        final Class<?> clazz = observer.getClass().getSuperclass();
         if (clazz != null && clazz == android.os.AsyncTask.class) {
-            int hashCode = observer.hashCode();
+            final int hashCode = observer.hashCode();
             currents.remove(hashCode);
-        }	    
+        }
+    }
+
+    /**
+     * Indicates that <code>initJs</code> method has been already called. That doesn't means the
+     * Hybridge is correctly working, only that the URL has been loaded in the WebView.
+     * 
+     * @return the boolean flag <code>isInitialized</code>
+     */
+    public boolean isInitialized() {
+        return isInitialized;
     }
 }
